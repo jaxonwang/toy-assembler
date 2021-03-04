3d50c6635c3152cd883c791469376f997b26cb2e
JX Wang <jxwang92@gmail.com>
2021-03-04 22:39:19 +0900

    Another day of doing nothing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+extern crate mpi;
+
+// use mpi::request::WaitGuard;
+use mpi::traits::*;
+use std::thread;
+use std::time;
+
+
 fn main() {
-    println!("Hello, world!");
+
+    let universe = mpi::initialize().unwrap();
+    let world = universe.world();
+    // let size = world.size();
+    let rank = world.rank();
+    println!("Hello, world! {}", rank);
+    thread::sleep(time::Duration::from_secs(10));
 }
