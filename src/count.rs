5748f89de0b1553a09428d641bae0e3d6fbddc1b
JX Wang <jxwang92@gmail.com>
2021-03-09 18:40:03 +0900

    finish couting feature

--- src/count.rs
+++ src/count.rs
@@ -2,19 +2,21 @@ use fastq::{Parser, Record, RefRecord};
 use std::collections::HashMap;
 // use std::env;
 // use std::fs;
+use std::convert::TryFrom;
 use std::io;
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 
-extern crate fastq;
-extern crate mpi;
-extern crate log;
-extern crate fern;
 extern crate chrono;
+extern crate crossbeam;
+extern crate fastq;
+extern crate fern;
+extern crate log;
+extern crate mpi;
 
-
+use log::{debug, info};
 use mpi::topology::SystemCommunicator;
 use mpi::traits::*;
-use log::{debug, info};
+use mpi::Threading;
 
 type CountNumber = u32;
 type KMer = Vec<u8>;
@@ -31,11 +33,7 @@ fn all_reads(count_table: &mut CountTable, kmer_len: usize, record: RefRecord) -
     for start_pos in 0..(read_slice.len() - kmer_len + 1) {
         let kmer = Vec::from(&read_slice[start_pos..start_pos + kmer_len]);
 
-        let count = match count_table.get(&kmer) {
-            Some(c) => c + 1,
-            None => 1,
-        };
-        count_table.insert(kmer, count);
+        update_count_table(count_table, kmer, 1);
     }
     return true;
 }
@@ -74,29 +72,57 @@ fn partition_by_borders<O: Ord>(kmer: &O, borders: &[O]) -> usize {
 
 mod mpiconst {
     pub const ROOT_RANK: mpi::topology::Rank = 0;
+    pub const DONE_MARK: &[u8] = "Done".as_bytes();
 }
-
-// fn dispatcher(count_table: CountTable, world: &SystemCommunicator, borders: &[KMer]) {
-//
-// }
-//
 
 struct KmerCounter {
     kmer_len: usize,
 }
 
+const COUNT_NUMBER_SIZE: usize = std::mem::size_of::<CountNumber>();
+type KCBytes = [u8; COUNT_NUMBER_SIZE];
+union KC {
+    count: CountNumber,
+    bytes: KCBytes,
+}
+
+fn pack(kmer: &KMer, num: CountNumber) -> Vec<u8> {
+    let mut buf = kmer.clone();
+    let kc = KC { count: num };
+    buf.append(&mut Vec::from(unsafe { kc.bytes }));
+    return buf;
+}
+
+fn unpack(buf: Vec<u8>) -> (KMer, CountNumber) {
+    let ha: [u8; 4] = <[u8; 4]>::try_from(&buf[buf.len() - COUNT_NUMBER_SIZE..]).unwrap();
+    let kc = KC { bytes: ha };
+    return (
+        buf.iter()
+            .take(buf.len() - COUNT_NUMBER_SIZE)
+            .cloned()
+            .collect(),
+        unsafe { kc.count },
+    );
+}
+
+fn update_count_table(count_table: &mut CountTable, kmer: KMer, count: CountNumber) {
+    let update_count = match count_table.get(&kmer) {
+        Some(c) => c + count,
+        None => count,
+    };
+    count_table.insert(kmer, update_count);
+}
+
 impl KmerCounter {
     pub fn distributed_count(&self, count_table: CountTable, world: &SystemCommunicator) {
-
         let myrank = world.rank();
         let p = world.size();
         let sample_num: usize = 10000 / p as usize; // total 10000 samples
-        let done_mark = "done".as_bytes();
         let root_process = world.process_at_rank(mpiconst::ROOT_RANK);
 
-        let mut borders:Vec<KMer> = vec![];
-        for _ in 0..p-1{
-            borders.push(vec![0u8;self.kmer_len]); // here I know the KMEr is u8 vec
+        let mut borders: Vec<KMer> = vec![];
+        for _ in 0..p - 1 {
+            borders.push(vec![0u8; self.kmer_len]); // here I know the KMEr is u8 vec
         }
 
         info!("start sampling.");
@@ -118,7 +144,7 @@ impl KmerCounter {
                     String::from_utf8_lossy(&msg[..]),
                     msg.len()
                 );
-                if &msg[..] == done_mark {
+                if &msg[..] == mpiconst::DONE_MARK {
                     done += 1;
                     debug!("all samples from {} are sent", s.source_rank());
                     continue;
@@ -150,7 +176,6 @@ impl KmerCounter {
                 borders.len() == p as usize - 1,
                 format!("Bad border! {} {} ", borders.len(), p)
             );
-
         } else {
             for (k, v) in count_table.iter().take(sample_num) {
                 for _ in 0..*v {
@@ -158,15 +183,77 @@ impl KmerCounter {
                 }
             }
             // send done
-            root_process.send(done_mark);
+            root_process.send(mpiconst::DONE_MARK);
         }
 
         debug!("broadcasting borders.");
-        for k in borders.iter_mut(){
+        for k in borders.iter_mut() {
             root_process.broadcast_into(&mut k[..]);
         }
         debug!("my borders:{:?}", &borders);
-        info!("sampling done");
+        info!("sampling done. Start counting.");
+
+        let mut new_table: CountTable = HashMap::new();
+        let local_table = Arc::new(Mutex::new(HashMap::new())); // stores entries partitioned to local
+
+        crossbeam::scope(|scope| {
+            scope.spawn(|_| self.dispatcher(count_table, world, borders, local_table.clone()));
+
+            let mut done = 0;
+            while done != p {
+                debug!("try_to receive");
+                let (msg, s) = world.any_process().receive_vec::<u8>();
+                if msg == mpiconst::DONE_MARK {
+                    done += 1;
+                    debug!("{} Done!", s.source_rank());
+                    continue;
+                }
+                let (kmer, count) = unpack(msg);
+                // debug!(
+                //     "receive {} from {}",
+                //     String::from_utf8_lossy(&kmer),
+                //     s.source_rank()
+                // );
+                update_count_table(&mut new_table, kmer, count);
+            }
+        })
+        .unwrap();
+        //
+        let guard = local_table.lock().unwrap();
+        for (k, v) in guard.iter() {
+            update_count_table(&mut new_table, k.clone(), *v);
+        }
+        info!("{:?}", &new_table);
+    }
+
+    fn dispatcher(
+        &self,
+        count_table: CountTable,
+        world: &SystemCommunicator,
+        borders: Vec<KMer>,
+        local_table: Arc<Mutex<CountTable>>,
+    ) {
+        let myrank = world.rank();
+        for (k, v) in count_table.iter() {
+            let dst = partition_by_borders(k, &borders[..]);
+            if dst == myrank as usize {
+                // if local set localtable
+                let mut guard = local_table.lock().unwrap();
+                update_count_table(&mut *guard, k.clone(), *v);
+                continue;
+            }
+            let dst_process = world.process_at_rank(dst as mpi::topology::Rank);
+            let msg = pack(k, *v);
+            // debug!(
+            //     "send {} to {}",
+            //     String::from_utf8_lossy(&msg),
+            //     dst
+            // );
+            dst_process.send(&msg[..]);
+        }
+        for i in 0..world.size() {
+            world.process_at_rank(i).send(mpiconst::DONE_MARK);
+        }
     }
 }
 
@@ -191,8 +278,7 @@ impl KmerCounter {
 // }
 //
 
-
-fn setup_logger(process_id:Arc<String>) -> Result<(), fern::InitError> {
+fn setup_logger(process_id: Arc<String>) -> Result<(), fern::InitError> {
     fern::Dispatch::new()
         .format(move |out, message, record| {
             out.finish(format_args!(
@@ -204,19 +290,20 @@ fn setup_logger(process_id:Arc<String>) -> Result<(), fern::InitError> {
                 message
             ))
         })
-        .level(log::LevelFilter::Debug)
+        .level(log::LevelFilter::Info)
         .chain(std::io::stdout())
         .apply()?;
     Ok(())
 }
 
 fn main() {
+    let (universe, threading) = mpi::initialize_with_threading(Threading::Multiple).unwrap();
+    assert_eq!(threading, mpi::environment::threading_support());
 
-    let universe = mpi::initialize().unwrap();
     let world = universe.world();
 
     let process_id = Arc::new(format!("P{}", world.rank()));
-    setup_logger(process_id.clone());
+    setup_logger(process_id.clone()).unwrap();
 
     let counter = KmerCounter { kmer_len: 3 };
     let in1 = "@
@@ -282,5 +369,15 @@ AAAAAEEE
         assert_eq!(partition_by_borders(&35, &borders[..]), 2);
         assert_eq!(partition_by_borders(&60, &borders[..]), 4);
         assert_eq!(partition_by_borders(&66, &borders[..]), 5);
+    }
+
+    #[test]
+    fn packing_test() {
+        let kmer: KMer = "ABCDE".as_bytes().to_vec();
+        let num = 123;
+        let buf = pack(&kmer, num);
+        let (kmer1, num1) = unpack(buf);
+        assert_eq!(num, num1);
+        assert_eq!(kmer1, kmer);
     }
 }
