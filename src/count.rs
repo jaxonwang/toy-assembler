7208f82fe0d41f7d4b497a68e137debd4b7580b2
JX Wang <jxwang92@gmail.com>
2021-03-12 17:11:13 +0900

    fix partition bugs

--- src/count.rs
+++ src/count.rs
@@ -63,14 +63,14 @@ fn _partition_by_hash(kmer: &KMer, partition_num: usize, window_size: usize) -> 
     let dohash = |window: &[u8]| {
         let mut hasher = std::collections::hash_map::DefaultHasher::new();
         window.hash(&mut hasher);
-        (hasher.finish() % partition_num as u64) as usize
+        hasher.finish()
     };
 
     let mut hashes = vec![];
     for start in 0..kmer.len() - window_size {
         hashes.push(dohash(&kmer[start..start + window_size]));
     }
-    *hashes.iter().min().unwrap()
+    (*hashes.iter().min().unwrap() % partition_num as u64) as usize
 }
 
 fn partition_by_hash(kmer: &KMer, partition_num: usize) -> usize {
@@ -274,7 +274,7 @@ impl KMerCounter {
         for (k, v) in guard.iter() {
             update_count_table(&mut new_table, k.clone(), *v);
         }
-        info!("Count Done");
+        info!("Count Done: kmers number {}", new_table.len());
         // for (k, v) in new_table.iter() {
         //     println!("{}: {:?}", String::from_utf8_lossy(k), v);
         // }
@@ -487,7 +487,7 @@ AAAAAEEE
         assert_eq!(partition_by_borders(&66, &borders[..]), 5);
     }
 
-    fn test_locality<F>(hash_func: F) -> i32
+    fn test_locality<F>(hash_func: F) -> (i32, f64)
     where
         F: Fn(&KMer, usize) -> usize,
     {
@@ -495,17 +495,27 @@ AAAAAEEE
         let read: Vec<u8> = (0..10000).map(|_| rng.gen::<u8>() % 4).collect();
         let mut kmers = vec![];
         let kmer_len = 31;
+        let partition_num = 10;
         for i in 0..read.len() - kmer_len {
             kmers.push(read[i..i + kmer_len].to_vec());
         }
-        let hashes: Vec<_> = kmers.iter().map(|kmer| hash_func(kmer, 10)).collect();
+        let hashes: Vec<usize> = kmers.iter().map(|kmer| hash_func(kmer, partition_num)).collect();
         let mut locality_counts = 0;
         for i in 0..hashes.len() - 1 {
             if hashes[i] != hashes[i + 1] {
                 locality_counts += 1;
             }
         }
-        locality_counts
+
+        let mut balance_count = vec![0usize; partition_num];
+        hashes.iter().map(|p|balance_count[*p]+=1).collect::<()>();
+        let mean = (balance_count.iter().sum::<usize>() / partition_num) as f64; 
+        let mut v1 =0f64;
+        for i in hashes.iter(){
+            v1 += (mean - *i as f64) * (mean - *i as f64);
+        }
+        let variance = v1.sqrt() / (partition_num - 1) as f64;
+        (locality_counts, variance)
     }
 
     #[test]
@@ -515,15 +525,15 @@ AAAAAEEE
             kmer.hash(&mut hasher);
             (hasher.finish() % partition_num as u64) as usize
         };
-        let locality_counts1 = test_locality(simplehash);
-        println!("count by hash: {}", locality_counts1);
-        let locality_counts2 = test_locality(partition_by_hash);
-        println!("count by locality hash: {}", locality_counts2);
+        let (locality_counts1, var1) = test_locality(simplehash);
+        println!("count by hash: {} {}", locality_counts1, var1);
+        let (locality_counts2, var2) = test_locality(partition_by_hash);
+        println!("count by locality hash: {} {}", locality_counts2, var2);
         assert!(locality_counts2 * 10 < locality_counts1);
         // benchmark code
         // for i in 5..30 {
-        //     let locality_counts2 = test_locality(|a, b| _partition_by_hash(a, b, i));
-        //     println!("{}, {}", i, locality_counts2);
+        //     let (c, v) = test_locality(|a, b| _partition_by_hash(a, b, i));
+        //     println!("{}, {}, {}", i, c, v);
         // }
     }
 
